99ae3975272f2a7ac3fad00a04f565d965cd74c5
tiago simao <tiagosimaorodri123@gmail.com>
2025-11-15 04:54:51 -0300

    commit test

--- src/main/java/br/com/pedidos/api/entity/ItemPedido.java
+++ src/main/java/br/com/pedidos/api/entity/ItemPedido.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 public class ItemPedido {
-
+// testando commit
 	private UUID id = UUID.randomUUID();
 	private Produto produto;
 	private Integer quantidade;
