a08a6962ca8ef375ce977567ec6ff5dc7961dd7b
tiago simao <tiagosimaorodri123@gmail.com>
2025-11-15 19:22:28 -0300

    feito a injeção no controller para o envio do pedido e reotrnar na tela.

--- src/main/java/br/com/pedidos/api/controller/PedidoController.java
+++ src/main/java/br/com/pedidos/api/controller/PedidoController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.pedidos.api.entity.Pedido;
+import br.com.pedidos.api.service.PedidoService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -23,6 +24,12 @@ public class PedidoController {
 	
 	private final Logger logger = LoggerFactory.getLogger(PedidoController.class);
 	
+	private final PedidoService pedidoService;
+	
+	public PedidoController(PedidoService pedidoService) {
+		this.pedidoService = pedidoService;
+	}
+	
 	@Operation(summary = "Cria um novo pedido", description = "Contém as operações para criar um novo pedido",
 				responses = @ApiResponse(responseCode = "201", description = "Recurso criado com sucesso!", 
 				content = @Content(mediaType = "application/json", schema = @Schema(implementation = Pedido.class))
@@ -32,7 +39,7 @@ public class PedidoController {
 	public ResponseEntity<Pedido> criarPedido(@RequestBody Pedido pedido ){
 		logger.info("Pedido recebido: {}", pedido.toString());
 		
-		System.out.println("pedido chegou" + pedido);
+		pedido = pedidoService.enfileirarPedido(pedido);
 		return ResponseEntity.status(HttpStatus.CREATED).body(pedido);
 		
 		
