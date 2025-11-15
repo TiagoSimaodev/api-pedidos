7c603483e15dc1e89d72227f6010b7aba303892d
tiago simao <tiagosimaorodri123@gmail.com>
2025-11-15 18:52:24 -0300

    foi documentando o endpoint utilizando as anotações da api swagger,

--- src/main/java/br/com/pedidos/api/controller/PedidoController.java
+++ src/main/java/br/com/pedidos/api/controller/PedidoController.java
@@ -10,13 +10,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.pedidos.api.entity.Pedido;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
+@Tag(name = "Pedidos", description = "Recursos para criar um novo pedido")
 @RestController
 @RequestMapping("/api/v1/pedidos")
 public class PedidoController {
 	
 	private final Logger logger = LoggerFactory.getLogger(PedidoController.class);
 	
+	@Operation(summary = "Cria um novo pedido", description = "Contém as operações para criar um novo pedido",
+				responses = @ApiResponse(responseCode = "201", description = "Recurso criado com sucesso!", 
+				content = @Content(mediaType = "application/json", schema = @Schema(implementation = Pedido.class))
+						)
+			)
 	@PostMapping
 	public ResponseEntity<Pedido> criarPedido(@RequestBody Pedido pedido ){
 		logger.info("Pedido recebido: {}", pedido.toString());
